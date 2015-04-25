9e50edc9067020c505e96363ae1b2556d77aa490
Raymond Burns <rburnsj@gmail.com>
2015-04-25 04:06:48 -0500

    Update file Copyright

--- includes/classes/breadcrumb.php
+++ includes/classes/breadcrumb.php
@@ -5,14 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2003 osCommerce
-  
-  Edited by 2014 Newburns Design and Technology
-  *************************************************
-  ************ New addon definitions **************
-  ************        Below          **************
-  *************************************************
-  SEO Header Tags Reloaded added -- http://addons.oscommerce.com/info/8864
+  Copyright (c) 2015 osCommerce
   
   Released under the GNU General Public License
 */
