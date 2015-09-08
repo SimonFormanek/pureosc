990a554d2ccf142f8928d59cd4482e5ac085d06e
Raymond Burns <rburnsj@gmail.com>
2015-09-08 16:41:40 -0500

    Integrate Mail Manager into checkout_process
    No more double emails after checkout only.
    Fixes #41

--- includes/modules/mail_manager/order_confirm.php
+++ includes/modules/mail_manager/order_confirm.php
@@ -5,13 +5,6 @@ mail manager for oscommerce
 Copyright (c) 2011 Niora http://www.css-oscommerce.com
 Released under the GNU General Public License
 */
-//get status of mail manager create account  email
-$mail_manager_status_query = tep_db_query("select status, template, htmlcontent, txtcontent from  " . TABLE_MM_RESPONSEMAIL . "  where mail_id = '1'");
-$mail_manager_status = tep_db_fetch_array($mail_manager_status_query);
-
-//default to tep_mail if order_confirm mailpiece inactived in admin
-if (isset($mail_manager_status['status']) && ($mail_manager_status['status'] == '1')) { 		
-
 // create the order totals variable
   for ($i=0, $n=sizeof($order_totals); $i<$n; $i++) {
     $mm_ordertotal .= strip_tags($order_totals[$i]['title']) . ' ' . strip_tags($order_totals[$i]['text']) . "\n".'<br/ >'; 
@@ -29,12 +22,15 @@ $output_subject = EMAIL_TEXT_CONFIRM.STORE_NAME;
 //define values for placeholder variables
 $order_no = EMAIL_TEXT_ORDER_NUMBER . ' ' . $insert_id;
 $order_date = EMAIL_TEXT_DATE_ORDERED . ' ' . strftime(DATE_FORMAT_LONG);
-if(!tep_session_is_registered('customer_is_guest')) {
-  $invoice_url = EMAIL_TEXT_INVOICE_URL . ' ' . tep_href_link(FILENAME_ACCOUNT_HISTORY_INFO, 'order_id=' . $insert_id, 'SSL', false);
-}
+
+// PWA Integration: Select Invoice URL if customer is a guest
 if(tep_session_is_registered('customer_is_guest')) {
 $invoice_url = EMAIL_WARNING . "\n\n";
+}else{
+  $invoice_url = EMAIL_TEXT_INVOICE_URL . ' ' . tep_href_link(FILENAME_ACCOUNT_HISTORY_INFO, 'order_id=' . $insert_id, 'SSL', false);
 }
+
+
 $delivery_address = tep_address_label($customer_id, $sendto, 0, '', '<br />');
 $billing_address = tep_address_label($customer_id, $billto, 0, '', '<br />');
 $order_comments = $order->info['comments'];
@@ -50,8 +46,4 @@ $output_content_txt=str_replace($placeholders, $values, $output_content_txt);
 
 //send email      
 tep_mm_sendmail($order->customer['firstname'] . ' ' . $order->customer['lastname'], $order->customer['email_address'], STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, $output_subject, $output_content_html, strip_tags($output_content_txt));	
-//if mail manager status update email 'inactive' process normally via oscommerce
-}else{						
-  	 tep_mail($order->customer['firstname'] . ' ' . $order->customer['lastname'], $order->customer['email_address'], EMAIL_TEXT_SUBJECT, $email_order, STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS);
-  }
 ?>
