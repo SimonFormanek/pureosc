2e5c7c79ae7573259006b22c63900f0267efdea1
Simon Formanek <mail@simonformanek.cz>
2015-10-20 06:23:22 +0200

    Bootstrap Home Page Carousel Banner Slider Module Plugin http://forums.oscommerce.com/topic/399709-bootstrap-home-page-carousel-banner-slider-module-plugin-complete-testing-welcome/

---
+++ includes/languages/czech/modules/content/header/cm_carousel.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_FRONT_PAGE_BANNER_ROTATOR_TITLE', 'Rotátor Banerů - Home Page');
+  define('MODULE_FRONT_PAGE_BANNER_ROTATOR_DESCRIPTION', 'Přidává rotátor bannerů na Home Page - vkládaných do Banerů.');
+  
+  
+  
+?>
---
+++ includes/languages/english/modules/content/header/cm_carousel.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_FRONT_PAGE_BANNER_ROTATOR_TITLE', 'Carousel Banners For Home Page');
+  define('MODULE_FRONT_PAGE_BANNER_ROTATOR_DESCRIPTION', 'Adds a carousel to Home Page with banner images set from Banner Manager.');
+  
+   
+?>
