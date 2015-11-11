743ac2bdbff9bc98bfb3ad2471b49365074f4bcc
Simon Formanek <mail@simonformanek.cz>
2015-11-11 11:22:01 +0100

    czech update admin/includes/languages/czech/categories.php

--- admin/includes/languages/czech/categories.php
+++ admin/includes/languages/czech/categories.php
@@ -31,11 +31,11 @@ define('TEXT_CATEGORIES', 'Categories:',true);
 define('TEXT_SUBCATEGORIES', 'Subcategories:',true);
 define('TEXT_PRODUCTS', 'Products:',true);
 define('TEXT_PRODUCTS_PRICE_INFO', 'Price:',true);
-define('TEXT_PRODUCTS_TAX_CLASS', 'Tax Class:',true);
+define('TEXT_PRODUCTS_TAX_CLASS', 'Sazba DPH:',true);
 define('TEXT_PRODUCTS_AVERAGE_RATING', 'Average Rating:',true);
 define('TEXT_PRODUCTS_QUANTITY_INFO', 'Quantity:',true);
-define('TEXT_DATE_ADDED', 'Date Added:',true);
-define('TEXT_DATE_AVAILABLE', 'Date Available:',true);
+define('TEXT_DATE_ADDED', 'Datum přidání do katalogu:',true);
+define('TEXT_DATE_AVAILABLE', 'Datum uvedení do prodeje:',true);
 define('TEXT_LAST_MODIFIED', 'Last Modified:',true);
 define('TEXT_IMAGE_NONEXISTENT', 'IMAGE DOES NOT EXIST',true);
 define('TEXT_NO_CHILD_CATEGORIES_OR_PRODUCTS', 'Please insert a new category or product in this level.',true);
@@ -45,59 +45,59 @@ define('TEXT_PRODUCT_DATE_AVAILABLE', 'This product will be in stock on %s.',tru
 
 define('TEXT_EDIT_INTRO', 'Please make any necessary changes',true);
 define('TEXT_EDIT_CATEGORIES_ID', 'Category ID:',true);
-define('TEXT_EDIT_CATEGORIES_NAME', 'Category Name:',true);
-define('TEXT_EDIT_CATEGORIES_IMAGE', 'Category Image:',true);
-define('TEXT_EDIT_SORT_ORDER', 'Sort Order:',true);
+define('TEXT_EDIT_CATEGORIES_NAME', 'Jméno kategorie/článku:',true);
+define('TEXT_EDIT_CATEGORIES_IMAGE', 'Obrázek:',true);
+define('TEXT_EDIT_SORT_ORDER', 'Pořadí:',true);
 
-define('TEXT_INFO_COPY_TO_INTRO', 'Please choose a new category you wish to copy this product to',true);
-define('TEXT_INFO_CURRENT_CATEGORIES', 'Current Categories:',true);
+define('TEXT_INFO_COPY_TO_INTRO', 'Vyberte kategorii do které se produkt zkopíruje',true);
+define('TEXT_INFO_CURRENT_CATEGORIES', 'Aktuální kategorie:',true);
 
-define('TEXT_INFO_HEADING_NEW_CATEGORY', 'New Category',true);
-define('TEXT_INFO_HEADING_EDIT_CATEGORY', 'Edit Category',true);
-define('TEXT_INFO_HEADING_DELETE_CATEGORY', 'Delete Category',true);
-define('TEXT_INFO_HEADING_MOVE_CATEGORY', 'Move Category',true);
-define('TEXT_INFO_HEADING_DELETE_PRODUCT', 'Delete Product',true);
-define('TEXT_INFO_HEADING_MOVE_PRODUCT', 'Move Product',true);
-define('TEXT_INFO_HEADING_COPY_TO', 'Copy To',true);
+define('TEXT_INFO_HEADING_NEW_CATEGORY', 'Nová kategorie/článek',true);
+define('TEXT_INFO_HEADING_EDIT_CATEGORY', 'Upravit kategorii/článek',true);
+define('TEXT_INFO_HEADING_DELETE_CATEGORY', 'Smazat kategorii/článek',true);
+define('TEXT_INFO_HEADING_MOVE_CATEGORY', 'Přesunout kategorii/článek',true);
+define('TEXT_INFO_HEADING_DELETE_PRODUCT', 'Smazat produkt',true);
+define('TEXT_INFO_HEADING_MOVE_PRODUCT', 'Přesunout produkt',true);
+define('TEXT_INFO_HEADING_COPY_TO', 'Kopírovat do',true);
 
-define('TEXT_DELETE_CATEGORY_INTRO', 'Are you sure you want to delete this category?',true);
-define('TEXT_DELETE_PRODUCT_INTRO', 'Are you sure you want to permanently delete this product?',true);
+define('TEXT_DELETE_CATEGORY_INTRO', 'Opravdu smazat kategorii/článek?',true);
+define('TEXT_DELETE_PRODUCT_INTRO', 'Opravdu smazat produkt?',true);
 
-define('TEXT_DELETE_WARNING_CHILDS', '<strong>WARNING:</strong> There are %s (child-)categories still linked to this category!',true);
-define('TEXT_DELETE_WARNING_PRODUCTS', '<strong>WARNING:</strong> There are %s products still linked to this category!',true);
+define('TEXT_DELETE_WARNING_CHILDS', '<strong>POZOR:</strong> Existuje %s vnořených kategoriií!',true);
+define('TEXT_DELETE_WARNING_PRODUCTS', '<strong>POZOR:</strong> Existuje %s produktů v této kategorii a jejích podkategoriích!',true);
 
 define('TEXT_MOVE_PRODUCTS_INTRO', 'Please select which category you wish <strong>%s</strong> to reside in',true);
 define('TEXT_MOVE_CATEGORIES_INTRO', 'Please select which category you wish <strong>%s</strong> to reside in',true);
 define('TEXT_MOVE', 'Move <strong>%s</strong> to:',true);
 
-define('TEXT_NEW_CATEGORY_INTRO', 'Please fill out the following information for the new category',true);
-define('TEXT_CATEGORIES_NAME', 'Category Name:',true);
-define('TEXT_CATEGORIES_IMAGE', 'Category Image:',true);
-define('TEXT_SORT_ORDER', 'Sort Order:',true);
+define('TEXT_NEW_CATEGORY_INTRO', 'Vyplňte údaje pro novou kategorii/článek',true);
+define('TEXT_CATEGORIES_NAME', 'Jméno:',true);
+define('TEXT_CATEGORIES_IMAGE', 'Obrázek:',true);
+define('TEXT_SORT_ORDER', 'Pořadí:',true);
 
-define('TEXT_PRODUCTS_STATUS', 'Products Status:',true);
-define('TEXT_PRODUCTS_DATE_AVAILABLE', 'Date Available:',true);
+define('TEXT_PRODUCTS_STATUS', 'Status:',true);
+define('TEXT_PRODUCTS_DATE_AVAILABLE', 'Datum uvedení do prodeje:',true);
 define('TEXT_PRODUCTS_CUSTOM_DATE','Datum (pro třídění):',true);
 define('TEXT_PRODUCTS_SORT_ORDER','Pořadí:',true);
-define('TEXT_PRODUCT_AVAILABLE', 'In Stock',true);
-define('TEXT_PRODUCT_NOT_AVAILABLE', 'Out of Stock',true);
-define('TEXT_PRODUCTS_MANUFACTURER', 'Products Manufacturer:',true);
-define('TEXT_PRODUCTS_NAME', 'Products Name:',true);
-define('TEXT_PRODUCTS_DESCRIPTION', 'Products Description:',true);
-define('TEXT_PRODUCTS_QUANTITY', 'Products Quantity:',true);
-define('TEXT_PRODUCTS_MODEL', 'Products Model:',true);
-define('TEXT_PRODUCTS_IMAGE', 'Products Image:',true);
-define('TEXT_PRODUCTS_MAIN_IMAGE', 'Main Image',true);
-define('TEXT_PRODUCTS_LARGE_IMAGE', 'Large Image',true);
-define('TEXT_PRODUCTS_LARGE_IMAGE_HTML_CONTENT', 'HTML Content (for popup)',true);
-define('TEXT_PRODUCTS_ADD_LARGE_IMAGE', 'Add Large Image',true);
-define('TEXT_PRODUCTS_LARGE_IMAGE_DELETE_TITLE', 'Delete Large Product Image?',true);
-define('TEXT_PRODUCTS_LARGE_IMAGE_CONFIRM_DELETE', 'Please confirm the removal of the large product image.',true);
-define('TEXT_PRODUCTS_URL', 'Products URL:',true);
-define('TEXT_PRODUCTS_URL_WITHOUT_HTTP', '<small>(without http://)</small>',true);
-define('TEXT_PRODUCTS_PRICE_NET', 'Products Price (Net):',true);
-define('TEXT_PRODUCTS_PRICE_GROSS', 'Products Price (Gross):',true);
-define('TEXT_PRODUCTS_WEIGHT', 'Products Weight:',true);
+define('TEXT_PRODUCT_AVAILABLE', 'Aktivní',true);
+define('TEXT_PRODUCT_NOT_AVAILABLE', 'Neaktivní',true);
+define('TEXT_PRODUCTS_MANUFACTURER', 'Výrobce:',true);
+define('TEXT_PRODUCTS_NAME', 'Název produktu:',true);
+define('TEXT_PRODUCTS_DESCRIPTION', 'Popis produktu:',true);
+define('TEXT_PRODUCTS_QUANTITY', 'Kusů skladem:',true);
+define('TEXT_PRODUCTS_MODEL', 'Model:',true);
+define('TEXT_PRODUCTS_IMAGE', 'Obrázek:',true);
+define('TEXT_PRODUCTS_MAIN_IMAGE', 'Základní obrázek',true);
+define('TEXT_PRODUCTS_LARGE_IMAGE', 'Velký obrázek',true);
+define('TEXT_PRODUCTS_LARGE_IMAGE_HTML_CONTENT', 'Popiska obrázku',true);
+define('TEXT_PRODUCTS_ADD_LARGE_IMAGE', 'Přidat velký obrázek',true);
+define('TEXT_PRODUCTS_LARGE_IMAGE_DELETE_TITLE', 'Smazat velký obrázek?',true);
+define('TEXT_PRODUCTS_LARGE_IMAGE_CONFIRM_DELETE', 'Potvrďte smazání velkého obrázku.',true);
+define('TEXT_PRODUCTS_URL', 'URL adresa produktu:',true);
+define('TEXT_PRODUCTS_URL_WITHOUT_HTTP', '<small>(bez http://)</small>',true);
+define('TEXT_PRODUCTS_PRICE_NET', 'Cena bez DPH:',true);
+define('TEXT_PRODUCTS_PRICE_GROSS', 'Cena s DPH:',true);
+define('TEXT_PRODUCTS_WEIGHT', 'Hmotnost produktu <br />(POZOR: nejedná-li se o produkt ke stažení nesmí být 0):',true);
 
 define('EMPTY_CATEGORY', 'Empty Category',true);
 
@@ -116,15 +116,15 @@ define('ERROR_CANNOT_MOVE_CATEGORY_TO_PARENT', 'Error: Category cannot be moved 
 ************************************************************************
 */
 // Definition for SEO Header Tags Reloaded
-  define('TEXT_CATEGORIES_DESCRIPTION', 'Category Description:<br><small>shows in category page</small>',true);
-  define('TEXT_EDIT_CATEGORIES_DESCRIPTION', 'Edit the Category Description:',true);
-  define('TEXT_PRODUCTS_SEO_TITLE', 'Products Title for SEO:<br><small>Replaces the product name in the &lt;title&gt; Meta Element.<br>Leave blank to default to product name.</small>',true);
-  define('TEXT_CATEGORIES_SEO_TITLE', 'Category Title for SEO:<br><small>Replaces the category name in the &lt;title&gt; Meta Element.<br>Leave blank to default to category name.</small>',true);
-  define('TEXT_EDIT_CATEGORIES_SEO_TITLE', 'Edit the Category Title for SEO:',true);
-  define('TEXT_CATEGORIES_SEO_DESCRIPTION', 'Category Meta Description for SEO:<br><small>Add a &lt;description&gt; Meta Element.</small>',true);
-  define('TEXT_EDIT_CATEGORIES_SEO_DESCRIPTION', 'Edit the Category Meta Description for SEO:<br><small>Changes the &lt;description&gt; Meta Element.</small>',true);
-  define('TEXT_CATEGORIES_SEO_KEYWORDS', 'Category Meta Keywords for SEO:<br><small>Add a &lt;keyword&gt; Meta Element.<br>Must be comma separated.</small>',true);
-  define('TEXT_EDIT_CATEGORIES_SEO_KEYWORDS', 'Edit the Category Meta Keywords for SEO:<br><small>Changes the &lt;keyword&gt; Meta Element.<br>Must be comma separated.</small>',true);
-  define('TEXT_PRODUCTS_SEO_DESCRIPTION', 'Product Meta Description for SEO:<br><small>Add a &lt;description&gt; Meta Element.</small>',true);
-  define('TEXT_PRODUCTS_SEO_KEYWORDS', 'Product Meta Keywords for SEO:<br><small>Add a &lt;keyword&gt; Meta Element.<br>Must be comma separated.</small>',true);
-  define('TEXT_PRODUCTS_MINI_DESCRIPTION', 'Product Mini Description:<br><small>Used in the "product list" in Category Pages.</small>',true);
+  define('TEXT_CATEGORIES_DESCRIPTION', 'Popis kategorie/text článku',true);
+  define('TEXT_EDIT_CATEGORIES_DESCRIPTION', 'Upravit popis kategorie/text článku:',true);
+  define('TEXT_PRODUCTS_SEO_TITLE', 'Titulek pro SEO (META TITLE):<br><small>Rozvedený název produktu, <br />nevyplněno - použije se název.<br />Nejdůježitější SEO prvek!</small>',true);
+  define('TEXT_CATEGORIES_SEO_TITLE', 'Titulek kategorie pro META hlavičku dokumentu:<br><small><br />nevypněno - použije se název kategorie.</small>',true);
+  define('TEXT_EDIT_CATEGORIES_SEO_TITLE', 'Upravit titulek kategorie pro META hlavičku:',true);
+  define('TEXT_CATEGORIES_SEO_DESCRIPTION', 'Popis kategorie/článku pro META hlavičku dokumentu (SEO, vyhledávání):',true);
+  define('TEXT_EDIT_CATEGORIES_SEO_DESCRIPTION', 'Upravit popis kategorie/článku pro META hlavičku:',true);
+  define('TEXT_CATEGORIES_SEO_KEYWORDS', 'Klíčová slova pro kategorii/článek:<br><small>Slova oddělujte čárkou.</small>',true);
+  define('TEXT_EDIT_CATEGORIES_SEO_KEYWORDS', 'Upravit klíčová slova pro kategorii/článek:<br><small>Slova oddělujte čárkou.</small>',true);
+  define('TEXT_PRODUCTS_SEO_DESCRIPTION', 'Popis produktu pro META hlavičku dokumentu (SEO, vyhledávání):',true);
+  define('TEXT_PRODUCTS_SEO_KEYWORDS', 'Meta klíčová slova (SEO, vyhledávání):<small><br />Slova oddělujte čárkou.</small>',true);
+  define('TEXT_PRODUCTS_MINI_DESCRIPTION', 'Stručný popis produktu:<small>Pro výpis kategorie</small>',true);
