65d793e92ee4c83433109dc45a814008c0cb964d
Simon Formanek <mail@simonformanek.cz>
2017-07-27 05:02:28 +0200

    bugfix link logo includes/modules/content/navigation/templates/cm_modular_navbar.php

--- includes/modules/content/navigation/templates/cm_modular_navbar.php
+++ includes/modules/content/navigation/templates/cm_modular_navbar.php
@@ -14,7 +14,7 @@
 <!-- Start cm_modular_navbar -->
 <nav class="navbar navbar-inverse navbar-no-corners navbar-no-margin" role="navigation">
   <div class="<?php echo BOOTSTRAP_CONTAINER; ?>">
-    <div class="navbar-header"><?php if (MODULE_CONTENT_NAVIGATION_MODULAR_NAVBAR_LOGO_ENABLED=='True') echo '<a class="fl navbar_logo" href="' . tep_href_link('/') . '">' . tep_image(DIR_WS_IMAGES . STORE_LOGO, STORE_NAME) . '</a>'; ?>
+    <div class="navbar-header"><?php if (MODULE_CONTENT_NAVIGATION_MODULAR_NAVBAR_LOGO_ENABLED=='True') echo '<a class="fl" href="' . tep_href_link(FILENAME_DEFAULT) . '">' . tep_image(DIR_WS_IMAGES . STORE_LOGO, STORE_NAME) . '</a>'; ?>
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-navbar-collapse-core-nav">
         <span class="sr-only"><?php echo HEADER_TOGGLE_NAV; ?></span>
         <span class="icon-bar"></span>
