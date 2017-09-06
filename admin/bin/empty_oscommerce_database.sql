d6035e43694c83c3a947e5e4123f3f43ca0799b6
Simon Formanek <mail@simonformanek.cz>
2017-09-06 02:39:26 +0200

    updated: empty_oscommerce_database.sql

--- admin/bin/empty_oscommerce_database.sql
+++ admin/bin/empty_oscommerce_database.sql
@@ -1,63 +1,66 @@
+#admin
+truncate administrators;
+truncate action_recorder;
 
 #truncate products & category tables:
-mysql_query('truncate products');
-mysql_query('truncate products_description');
-mysql_query('truncate products_description_versions');
-mysql_query('truncate categories_description');
-mysql_query('truncate categories');
-mysql_query('truncate products_to_categories');
-mysql_query('truncate products_images');
+truncate products;
+truncate products_description;
+truncate products_description_versions;
+truncate categories_description;
+truncate categories;
+truncate products_to_categories;
+truncate products_images;
 
 #reviews
-mysql_query('truncate reviews');
-mysql_query('truncate reviews_description');
+truncate reviews;
+truncate reviews_description;
 #options
-mysql_query('truncate products_options');
-mysql_query('truncate products_options_values');
-mysql_query('truncate products_options_values_to_products_options');
+truncate products_options;
+truncate products_options_values;
+truncate products_options_values_to_products_options;
 #attributes
-mysql_query('truncate products_attributes');
-mysql_query('truncate products_attributes_download');
+truncate products_attributes;
+truncate products_attributes_download;
 
 #specials (discounts)
-mysql_query('truncate specials');
+truncate specials;
 
 #manufacturers
-mysql_query('truncate manufacturers');
-mysql_query('truncate  manufacturers_info');
+truncate manufacturers;
+truncate  manufacturers_info;
 
 
 #truncate customers
-mysql_query('truncate customers');
-mysql_query('truncate customers_real');
-mysql_query('truncate customers_info');
-mysql_query('truncate address_book');
-mysql_query('truncate address_book_real');
-mysql_query('truncate products_notifications');
+truncate customers;
+truncate customers_real;
+truncate customers_info;
+truncate address_book;
+truncate address_book_real;
+truncate products_notifications;
 
 
 
 # orders
-mysql_query('truncate orders');
-mysql_query('truncate orders_real');
-mysql_query('truncate orders_products');
-mysql_query('truncate orders_products_attributes');
-mysql_query('truncate orders_products_download');
-mysql_query('truncate orders_status_history');
-mysql_query('truncate orders_total');
+truncate orders;
+truncate orders_real;
+truncate orders_products;
+truncate orders_products_attributes;
+truncate orders_products_download;
+truncate orders_status_history;
+truncate orders_total;
 
 
-mysql_query('truncate sessions');
+truncate sessions;
 
-mysql_query('truncate customers_basket');
-mysql_query('truncate whos_online');
+truncate customers_basket;
+truncate whos_online;
 
-mysql_query('truncate banners');
-mysql_query('truncate banners_history');
+truncate banners;
+truncate banners_history;
 
 
-mysql_query('truncate banned_ip');
-mysql_query('truncate phpids_intrusions');
+truncate banned_ip;
+truncate phpids_intrusions;
 
-mysql_query('truncate administrators');
-mysql_query('truncate action_recorder');
+truncate administrators;
+truncate action_recorder;
