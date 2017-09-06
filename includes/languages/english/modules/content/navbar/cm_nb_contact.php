feb5638a956a6f3a2d1cae8260384a22aa50140c
Simon Formanek <mail@simonformanek.cz>
2017-09-07 01:21:23 +0200

    PURE:NEW modular navbar ContactUs module

---
+++ includes/languages/english/modules/content/navbar/cm_nb_contact.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id cm_nb_contact.php v1.0 20160215 Kymation $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 James C Keebaugh
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_NAVBAR_CONTACT_TITLE', 'Navbar Contact Us');
+  define('MODULE_CONTENT_NAVBAR_CONTACT_DESCRIPTION', 'Show the contact us link in the navbar.');
+
+  define('HEADER_CONTACT_US', '<i class="fa fa-envelope"></i><span class="hidden-sm"> Contact Us</span>');
---
+++ includes/modules/content/navbar/templates/cm_nb_contact.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+  $Id cm_nb_contact.php v1.0 20160215 Kymation $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 James C Keebaugh
+
+  Released under the GNU General Public License
+*/
+?>
+
+<!-- Start cm_nb_contact -->
+<li><a href="<?php echo tep_href_link( FILENAME_CONTACT_US ); ?>"><?php echo HEADER_CONTACT_US; ?></a></li>
+<!-- End cm_nb_contact -->
