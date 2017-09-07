fd403ff1392bdcbfa14233eb820bc4283536e285
Simon Formanek <mail@simonformanek.cz>
2017-09-08 01:51:11 +0200

    PURE:FIX cm_footer_information_links.php template for autogenerated information (includes/modules/content/footer/templates/links.php)

--- includes/modules/content/footer/templates/links.php
+++ includes/modules/content/footer/templates/links.php
@@ -2,11 +2,9 @@
   <div class="footerbox information">
     <h2><?php echo MODULE_CONTENT_FOOTER_INFORMATION_HEADING_TITLE; ?></h2>
     <ul class="nav nav-pills nav-stacked">
-      <li><a href="<?php echo tep_href_link('shipping.php'); ?>"><?php echo MODULE_CONTENT_FOOTER_INFORMATION_SHIPPING; ?></a></li>
-      <li><a href="<?php echo tep_href_link('privacy.php'); ?>"><?php echo MODULE_CONTENT_FOOTER_INFORMATION_PRIVACY; ?></a></li>
-      <li><a href="<?php echo tep_href_link('conditions.php'); ?>"><?php echo MODULE_CONTENT_FOOTER_INFORMATION_CONDITIONS; ?></a></li>
-      <li><a href="<?php echo tep_href_link('contact_us.php'); ?>"><?php echo MODULE_CONTENT_FOOTER_INFORMATION_CONTACT; ?></a></li>
-      <?php
+  <?php echo tep_information_show_category(1);?>
+  <?php //echo  '<li><a href="' . tep_href_link('contact_us.php') . '">' . MODULE_CONTENT_FOOTER_INFORMATION_CONTACT . '</a></li>';
+
       /*** Begin Article Manager ****/
       echo $aLinks; 
       /*** End Article Manager ****/
