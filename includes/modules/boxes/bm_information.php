17c12ad453428cfa94d8f6f18137a26e84e3a218
Simon Formanek <mail@simonformanek.cz>
2017-09-08 01:47:28 +0200

    modules/boxes/bm_information.php: comments cleanup

--- includes/modules/boxes/bm_information.php
+++ includes/modules/boxes/bm_information.php
@@ -42,9 +42,6 @@
               '  <div class="ui-widget-header infoBoxHeading">' . MODULE_BOXES_INFORMATION_BOX_TITLE . '</div>' .
               '  <div class="ui-widget-content infoBoxContents">' .
                    tep_information_show_category(1) .
-//              '    <a href="' . tep_href_link(FILENAME_SHIPPING) . '">' . MODULE_BOXES_INFORMATION_BOX_SHIPPING . '</a><br />' .
-//              '    <a href="' . tep_href_link(FILENAME_PRIVACY) . '">' . MODULE_BOXES_INFORMATION_BOX_PRIVACY . '</a><br />' .
-//              '    <a href="' . tep_href_link(FILENAME_CONDITIONS) . '">' . MODULE_BOXES_INFORMATION_BOX_CONDITIONS . '</a><br />' .
               '    <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">' . MODULE_BOXES_INFORMATION_BOX_CONTACT . '</a>' .
               '  </div>' .
               '</div>';
