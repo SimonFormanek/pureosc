565370a297233fe899d76886f9ac7f3d3ebf6fd4
Simon Formanek <mail@simonformanek.cz>
2017-10-09 03:43:29 +0200

    new oscconfig with variables NOW simple!

--- admin/ext/oscconfig/admin/configure.php
+++ admin/ext/oscconfig/admin/configure.php
@@ -1,26 +1,24 @@
 <?php
-  define('DIR_FS_DOCUMENT_ROOT', '/home/f/git/osc/osc/'); // <------ need to bee configured
-
-//  define('SHOP_KEYS_PATH','/home/printondemand/shop_keys/'); //SECURITY WARNING: need to bee obfuscated 
-//  define('ADMIN_PRIVATE_KEYS_PATH','/home/printondemand/admin_private_keys/'); //SECURITY WARNING: on production environement save keys on removable media
+define('DIR_FS_MASTER_ROOT_DIR','/home/f/git/osc/osc/');
+  define('ENABLE_SSL_CATALOG', false);
+  define('ENABLE_SSL', false);
   define('SERVER_INSTANCE','admin'); // admin or empty for shop ???? TODO
 
-  define('HTTP_SERVER', 'http://osc.local'); // <------ need to bee configured
-  define('HTTPS_SERVER', 'http://osc.local'); // <------ need to bee configured
-  define('ENABLE_SSL', false);
+  define('HTTP_SERVER', 'http://' . $_SERVER['HTTP_HOST']); // <------ need to bee configured
+  define('HTTPS_SERVER', 'https://' . $_SERVER['HTTP_HOST']); // <------ need to bee configured
   define('HTTP_COOKIE_DOMAIN', '');
   define('HTTPS_COOKIE_DOMAIN', '');
   define('HTTP_COOKIE_PATH', '/admin');
   define('HTTPS_COOKIE_PATH', '/admin');
-  define('HTTP_CATALOG_SERVER', 'http://osc.local'); // <------ need to bee configured
-  define('HTTPS_CATALOG_SERVER', 'http://osc.local'); // <------ need to bee configured
-  define('ENABLE_SSL_CATALOG', 'true');
+  define('HTTP_CATALOG_SERVER', 'http://' . $_SERVER['HTTP_HOST']); // <------ need to bee configured
+  define('HTTPS_CATALOG_SERVER', 'https://' . $_SERVER['HTTP_HOST']); // <------ need to bee configured
+  define('DIR_FS_DOCUMENT_ROOT', DIR_FS_MASTER_ROOT_DIR); // <------ need to bee configured
   define('DIR_WS_ADMIN', '/admin/');
   define('DIR_WS_HTTPS_ADMIN', '/admin/');// <------ need to bee configured SECURITY: obfuscation
-  define('DIR_FS_ADMIN', '/home/f/git/osc/osc/admin/'); //  <------ need to bee configured SECURITY: obfuscation
+  define('DIR_FS_ADMIN', DIR_FS_MASTER_ROOT_DIR . 'admin/'); //  <------ need to bee configured SECURITY: obfuscation
   define('DIR_WS_CATALOG', '/');
   define('DIR_WS_HTTPS_CATALOG', '/');
-  define('DIR_FS_CATALOG', '/home/f/git/osc/osc/'); // <------ need to bee configured
+  define('DIR_FS_CATALOG', DIR_FS_MASTER_ROOT_DIR); // <------ need to bee configured
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
@@ -32,7 +30,7 @@
   define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
   define('DIR_WS_CATALOG_LANGUAGES', DIR_WS_CATALOG . 'includes/languages/');
   define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
-  define('DIR_FS_CATALOG_IMAGES', '/home/f/git/osc/osc/images/'); // <------ need to bee configured
+  define('DIR_FS_CATALOG_IMAGES', DIR_FS_MASTER_ROOT_DIR . 'images/'); // <------ need to bee configured
   define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
   define('DIR_FS_BACKUP', DIR_FS_ADMIN . '../../data/backups/');
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
@@ -41,7 +39,6 @@
 //  define('MULTI_DATABASE','false'); //true if defined multipe databases
 //local db
 
-  define('DB_SERVER_USERNAME_PREFIX','os'); //2 chars
   define('USE_PCONNECT', 'false');
   define('STORE_SESSIONS', 'mysql');
   define('CFG_TIME_ZONE', 'Europe/Prague'); // <------ need to bee configured
@@ -52,3 +49,7 @@ $imgWidthSmall = 64;
 //unused:$imgFixedHeight = 190; //vyska obr pro tapetu
 //unused:$imgFixedHeightSmall = 110; //vyska obr pro tapetu MALY
 //define('DEFAULT_PRODUCT_TEMPLATE','2'); //1 = product 2 = arricle
+
+//  define('SHOP_KEYS_PATH','/home/printondemand/shop_keys/'); //SECURITY WARNING: need to bee obfuscated 
+//  define('ADMIN_PRIVATE_KEYS_PATH','/home/printondemand/admin_private_keys/'); //SECURITY WARNING: on production environement save keys on removable media
+//  define('DB_SERVER_USERNAME_PREFIX','os'); //2 chars
