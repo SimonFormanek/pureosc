a7eda741054cdf85dc202f0720add21fcf38d199
Simon Formanek <mail@simonformanek.cz>
2017-11-01 18:41:49 +0100

    BUGFIX: gallery images product_info.php missing js

--- product_info.php
+++ product_info.php
@@ -162,6 +162,37 @@
       }
     }
 ?>
+<script type="text/javascript">
+$(function() {
+  $('#piGal').css({
+    'visibility': 'hidden'
+  });
+
+  $('#piGal').photosetGrid({
+    layout: '<?php echo $photoset_layout; ?>',
+    width: '250px',
+    highresLinks: true,
+    rel: 'pigallery',
+    onComplete: function() {
+      $('#piGal').css({ 'visibility': 'visible'});
+
+      $('#piGal a').colorbox({
+        maxHeight: '90%',
+        maxWidth: '90%',
+        rel: 'pigallery'
+      });
+
+      $('#piGal img').each(function() {
+        var imgid = $(this).attr('id').substring(9);
+
+        if ( $('#piGalDiv_' + imgid).length ) {
+          $(this).parent().colorbox({ inline: true, href: "#piGalDiv_" + imgid });
+        }
+      });
+    }
+  });
+});
+</script>
 
 <div itemprop="description">
   <?php echo stripslashes($product_info['products_description']); ?>
