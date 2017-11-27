56d69ea026dc82436b28bdaf6b52d8d0d0f44be8
Simon Formanek <mail@simonformanek.cz>
2017-11-27 04:53:17 +0100

    cesinaFIX

--- includes/languages/czech/modules/content/checkout_success/cm_cs_thank_you.php
+++ includes/languages/czech/modules/content/checkout_success/cm_cs_thank_you.php
@@ -15,7 +15,7 @@
 
   define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_SUCCESS', 'Vaše objednávka byla úspěšně zpracována. Zboží bude expedováno nejbližší pracovní den.',true);
   define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_SEE_ORDERS', 'Stav zpracování objednávky zobrazíte na stránce <a class="btn btn-success" role="button" href="%s">Historie objednávek</a>',true);
-  define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_CONTACT_STORE_OWNER', 'Máte-li jakkoli dotazy využijte <a class="btn btn-info" role="button" href="%s">kontaktní formulář</a> stránka.',true);
+  define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_CONTACT_STORE_OWNER', 'Máte-li jakýkoli dotaz, využijte <a class="btn btn-info" role="button" href="%s">kontaktní formulář</a>, nebo nám napište <a href="mailto:' . STORE_OWNER_EMAIL_ADDRESS . '">' . STORE_OWNER_EMAIL_ADDRESS . '</a> stránka.',true);
 
   define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_THANKS_FOR_SHOPPING', '<strong>Děkujeme za vaši objednávku.</strong>',true);
 ?>
