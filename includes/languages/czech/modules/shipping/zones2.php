aeeddbd4847963748947c9aac61fdb0d6b898285
Simon Formanek <mail@simonformanek.cz>
2017-12-09 19:09:59 +0100

    includes/modules/shipping/zones2.php second copy of zones module for express shipping

---
+++ includes/languages/czech/modules/shipping/zones2.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id: zones.php,v 1.3 2002/11/19 01:48:08 dgw_ Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_ZONES2_TEXT_TITLE', 'Zone Based Express Shipping Rates');
+define('MODULE_SHIPPING_ZONES2_TEXT_DESCRIPTION', 'Zone Based Rates Express');
+define('MODULE_SHIPPING_ZONES2_TEXT_WAY', 'Air'); //no longer used
+define('MODULE_SHIPPING_ZONES2_TEXT_UNITS', 'lb(s)');
+define('MODULE_SHIPPING_ZONES2_INVALID_ZONE', 'No shipping available to the selected country');
+define('MODULE_SHIPPING_ZONES2_UNDEFINED_RATE', 'The shipping rate cannot be determined at this time');
+?>
