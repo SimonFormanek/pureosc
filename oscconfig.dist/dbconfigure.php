90d2e7f11e86fbebdcacd1ffbb780b6fc771135d
Cybervitexus <info@vitexsoftware.cz>
2018-04-08 00:57:39 +0200

    EaseFramework SQL constants Makefile update

--- oscconfig.dist/dbconfigure.php
+++ oscconfig.dist/dbconfigure.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //username definition: 2 chars DB_SERVER_USERNAME_PREFIX + 3 chars acces level + 11 chars salt
 //admin users acces level roles: 
 //xx=2 chars db prefix, 1234567890 = salt
@@ -10,10 +10,16 @@
 //xxadm1234567890 = ordinary admin granted to view orders
 //xxmad1234567890 = master admin granted to add functions i.e.: grant all on dbname.* to dbmad@localhost identified by 'password' with grant option;
 //username examples: poadm1234567890 = ordinary admin, pomad12345678901 = master admin
-
 //master server
 //  define('MULTI_DATABASE','false'); //true if defined multiple databases
-  define('DB_SERVER', 'localhost');
-  define('DB_DATABASE', 'pureosc');
-  define('DB_SERVER_USERNAME', 'root');
-  define('DB_SERVER_PASSWORD', '');
+define('DB_SERVER', 'localhost');
+define('DB_DATABASE', 'pureosc');
+define('DB_SERVER_USERNAME', 'root');
+define('DB_SERVER_PASSWORD', '');
+
+
+//EasePHP Framework
+define('DB_HOST', constant('DB_SERVER'));
+define('DB_PASSWORD', constant('DB_SERVER_PASSWORD'));
+define('DB_USERNAME', constant('DB_SERVER_USERNAME'));
+define('DB_TYPE', 'mysql');
