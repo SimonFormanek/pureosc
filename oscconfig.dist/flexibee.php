071be7c14c273d8d7c7de6da5e6a0e500c051250
Simon Formanek <mail@simonformanek.cz>
2019-01-02 17:41:06 +0100

    added flexibee to oscconfig.dist

---
+++ oscconfig.dist/flexibee.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+if (!defined('EASE_APPNAME')) {
+    define('EASE_APPNAME', 'PureOSC');
+}
+if (!defined('EASE_LOGGER')) {
+    define('EASE_LOGGER', 'syslog');
+}
+//Uncomment this to owerride DB settings:
+/*
+define('FLEXIBEE_URL', '');
+define('FLEXIBEE_LOGIN', '');
+define('FLEXIBEE_PASSWORD', '');
+define('FLEXIBEE_COMPANY', '');
+*/
