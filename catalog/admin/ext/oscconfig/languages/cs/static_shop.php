9a89bb35b74c0ab3a93dcf43ad7ad1cc50a25545
Simon Formanek <mail@simonformanek.cz>
2019-01-27 22:21:15 +0100

    admin-oscconfig-removed

--- catalog/admin/ext/oscconfig/languages/cs/static_shop.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SERVER_INSTANCE','shop'); // admin|shop
-define('HTTPS_SERVER', 'http://osc.local'); //no thirth domain = use default language; en.domain.com, cs.domain.com
-define('HTTP_SERVER', HTTPS_SERVER); 
-define('RSYNC_LOCAL_DEST_PATH','/home/osc/WWW/'); //trailing slash! '/'
-define('RSYNC_REMOTE_DEST_DIR','/home/osc/WWW/');
