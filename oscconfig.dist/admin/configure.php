64d0723450dd81ea8806d78dab7935e93eb8d663
Simon Formanek <mail@simonformanek.cz>
2019-01-27 02:32:07 +0100

    best configure.php+admin

--- oscconfig.dist/admin/configure.php
+++ oscconfig.dist/admin/configure.php
@@ -1,50 +1,56 @@
 <?php
-	define('CATALOG_DOMAIN',''); //eg www.my-shop.com
-  define('DIR_FS_MASTER_ROOT_DIR','/var/www/'); //trailing slash YES, catalog subdir/catalog NO (!)
-  define('ENABLE_SSL_CATALOG', false);
-  define('ENABLE_SSL', false);
-  define('SERVER_INSTANCE','admin'); // admin or empty for shop ???? TODO
+define('CATALOG_DOMAIN', ''); //not empty !
+define('DIR_FS_MASTER_ROOT_DIR', '/var/www/'); //trailing slash YES, catalog subdir/catalog NO (!)
+define('SERVER_INSTANCE', 'admin'); // admin or empty for shop ???? TODO
 
-  define('HTTP_SERVER', 'http://' . $_SERVER['HTTP_HOST']); // <------ need to bee configured
-  define('HTTPS_SERVER', 'https://' . $_SERVER['HTTP_HOST']); // <------ need to bee configured
-  define('HTTP_COOKIE_DOMAIN', '');
-  define('HTTPS_COOKIE_DOMAIN', '');
-  define('HTTP_COOKIE_PATH', '/admin');
-  define('HTTPS_COOKIE_PATH', '/admin');
-  define('HTTP_CATALOG_SERVER', 'http://' . CATALOG_DOMAIN); // <------ need to bee configured
-  define('HTTPS_CATALOG_SERVER', 'https://' . CATALOG_DOMAIN); // <------ need to bee configured
-  define('DIR_FS_DOCUMENT_ROOT', DIR_FS_MASTER_ROOT_DIR . 'catalog/'); // <------ need to bee configured
-  define('DIR_WS_ADMIN', '/admin/');
-  define('DIR_WS_HTTPS_ADMIN', '/admin/');// <------ need to bee configured SECURITY: obfuscation
-  define('DIR_FS_ADMIN', DIR_FS_MASTER_ROOT_DIR . 'admin/'); //  <------ need to bee configured SECURITY: obfuscation
-  define('DIR_WS_CATALOG', '/');
-  define('DIR_WS_HTTPS_CATALOG', '/');
-  define('DIR_FS_CATALOG', DIR_FS_MASTER_ROOT_DIR . '/catalog/'); // <------ need to bee configured
-  define('DIR_WS_IMAGES', 'images/');
-  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
-  define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
-  define('DIR_WS_INCLUDES', 'includes/');
-  define('DIR_WS_BOXES', DIR_WS_INCLUDES . 'boxes/');
-  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
-  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
-  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
-  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
-  define('DIR_WS_CATALOG_LANGUAGES', DIR_WS_CATALOG . 'includes/languages/');
-  define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
-  define('DIR_FS_CATALOG_IMAGES', DIR_FS_MASTER_ROOT_DIR . 'images/'); // <------ need to bee configured
-  define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
-  define('DIR_FS_BACKUP', DIR_FS_ADMIN . '../../data/backups/');
-  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
-  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
-  define('WEBMASTER_EMAIL', '00420602604992@sms.cz.o2.com');
+if ($_SERVER['REQUEST_SCHEME'] =='https') {
+  $https = 'https';
+  define('ENABLE_SSL', true); // <------ need to bee configured
+  define('ENABLE_SSL_CATALOG', true);
+} else {
+  $https = 'http';
+  define('ENABLE_SSL', false); // <------ need to bee configured
+  define('ENABLE_SSL_CATALOG', false);
+}
+define('HTTP_SERVER', $https . '://' . $_SERVER['HTTP_HOST']);
+define('HTTPS_SERVER', 'https://' . $_SERVER['HTTP_HOST']); // <------ need to bee configured
+define('HTTP_COOKIE_DOMAIN', '');
+define('HTTPS_COOKIE_DOMAIN', '');
+define('HTTP_COOKIE_PATH', '/admin');
+define('HTTPS_COOKIE_PATH', '/admin');
+define('HTTP_CATALOG_SERVER', 'http://' . CATALOG_DOMAIN); // <------ need to bee configured
+define('HTTPS_CATALOG_SERVER', 'https://' . CATALOG_DOMAIN); // <------ need to bee configured
+define('DIR_FS_DOCUMENT_ROOT', DIR_FS_MASTER_ROOT_DIR . 'catalog/'); // <------ need to bee configured
+define('DIR_WS_ADMIN', '/admin/');
+define('DIR_WS_HTTPS_ADMIN', '/admin/'); // <------ need to bee configured SECURITY: obfuscation
+define('DIR_FS_ADMIN', DIR_FS_MASTER_ROOT_DIR . 'catalog/admin/'); //  <------ need to bee configured SECURITY: obfuscation
+define('DIR_WS_CATALOG', '/');
+define('DIR_WS_HTTPS_CATALOG', '/');
+define('DIR_FS_CATALOG', DIR_FS_MASTER_ROOT_DIR . '/catalog/'); // <------ need to bee configured
+define('DIR_WS_IMAGES', 'images/');
+define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
+define('DIR_WS_INCLUDES', 'includes/');
+define('DIR_WS_BOXES', DIR_WS_INCLUDES . 'boxes/');
+define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+define('DIR_WS_CATALOG_LANGUAGES', DIR_WS_CATALOG . 'includes/languages/');
+define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
+define('DIR_FS_CATALOG_IMAGES', DIR_FS_MASTER_ROOT_DIR . 'images/'); // <------ need to bee configured
+define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
+define('DIR_FS_BACKUP', DIR_FS_ADMIN . '../../data/backups/');
+define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+define('WEBMASTER_EMAIL', '00420602604992@sms.cz.o2.com');
 
 //  define('MULTI_DATABASE','false'); //true if defined multipe databases
 //local db
 
-  define('USE_PCONNECT', 'false');
-  define('STORE_SESSIONS', 'mysql');
-  define('CFG_TIME_ZONE', 'Europe/Prague'); // <------ need to bee configured
-
+define('USE_PCONNECT', 'false');
+define('STORE_SESSIONS', 'mysql');
+define('CFG_TIME_ZONE', 'Europe/Prague'); // <------ need to bee configured
 //SMAZAT? new_products stripe generator (catgories.php)
 $imgWidth = 110;
 $imgWidthSmall = 64;
