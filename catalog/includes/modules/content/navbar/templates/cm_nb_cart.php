54d736f707b81af2b35fa9b111a792d3b12a2e3a
Cybervitexus <info@vitexsoftware.cz>
2019-01-30 05:00:48 +0100

    slovak navbar gettextized

--- catalog/includes/modules/content/navbar/templates/cm_nb_cart.php
+++ catalog/includes/modules/content/navbar/templates/cm_nb_cart.php
@@ -13,38 +13,29 @@
 
 <!-- Start cm_nb_cart -->
 <li class="dropdown">
-    <?php
-    
-    
-    if ($cart->count_contents() > 0) { ?>
+    <?php if ($cart->count_contents() > 0) { ?>
         <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php
-            echo sprintf(HEADER_CART_CONTENTS.' ', $cart->count_contents(),
-                $cart->count_contents());
-            ?></a>
+        echo '<i class="fa fa-shopping-cart"></i> '.sprintf(_('%s item(s)'),
+            $cart->count_contents()).' <span class="caret"></span> ';
+        ?></a>
         <ul class="dropdown-menu">
             <li><a href="<?php echo tep_href_link(FILENAME_SHOPPING_CART); ?>"><?php
-                    echo sprintf(HEADER_CART_HAS_CONTENTS,
-                        $cart->count_contents(),
-                        $currencies->format($cart->show_total()));
-                    ?></a></li>
+        echo sprintf(_('%s item(s)'), $cart->count_contents()).
+        ', '.$currencies->format($cart->show_total());
+        ?></a></li>
             <li class="divider"></li>
-            <li><a href="<?php echo tep_href_link(FILENAME_SHOPPING_CART); ?>"><?php echo HEADER_CART_VIEW_CART; ?></a></li>
+            <li><a href="<?php echo tep_href_link(FILENAME_SHOPPING_CART); ?>"><?php echo _('View cart'); ?></a></li>
         </ul>
     </li>
 
-    <li><a href="<?php echo tep_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL'); ?>"><?php echo HEADER_CART_CHECKOUT; ?></a></li>
+    <li><a href="<?php echo tep_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL'); ?>"><i class="fa fa-angle-right"></i> <?php echo _('Checkout'); ?></a></li>
 
 <?php } else { ?>
 
-    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo HEADER_CART_NO_CONTENTS; ?></a>
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-shopping-cart"></i> <?php echo _('0 items'); ?></a>
     <ul class="dropdown-menu">
-
-        <li><a href="<?php echo tep_href_link(FILENAME_SHOPPING_CART); ?>"><?php echo HEADER_CART_VIEW_CART; ?></a></li>
+        <li><a href="<?php echo tep_href_link(FILENAME_SHOPPING_CART); ?>"><?php echo _('View Cart'); ?></a></li>
     </ul>
     </li>
-
-
-
-        <!--ORIG:            <li class="nav navbar-text"><?php echo HEADER_CART_NO_CONTENTS; ?></li>-->
 <?php } ?>
 <!-- End cm_nb_cart -->
