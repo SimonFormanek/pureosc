47090fe86919cdeef56d7d80317fa749b4158380
Cybervitexus <info@vitexsoftware.cz>
2019-01-30 04:29:27 +0100

    account menu to i18n

--- catalog/includes/modules/content/navbar/templates/cm_nb_account.php
+++ catalog/includes/modules/content/navbar/templates/cm_nb_account.php
@@ -13,29 +13,30 @@
 
 <!-- Start cm_nb_account -->
 <li class="dropdown">
-    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo (tep_session_is_registered('customer_id'))
-        ? sprintf(HEADER_ACCOUNT_LOGGED_IN, $customer_first_name) : HEADER_ACCOUNT_LOGGED_OUT;
-?></a>
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php
+        echo (tep_session_is_registered('customer_id')) ? sprintf('<i class="fa fa-user"></i> %s <span class="caret"></span>',
+                $customer_first_name) : '<i class="glyphicon glyphicon-user"></i><span class="hidden-sm"> '._('My Account').'</span> <span class="caret"></span>';
+        ?></a>
     <ul class="dropdown-menu">
 
         <?php if (tep_session_is_registered('customer_id')) { ?>
-            <li><a href="<?php echo tep_href_link(FILENAME_LOGOFF, '', 'SSL'); ?>"><?php echo HEADER_ACCOUNT_LOGOFF; ?></a></li>
-<?php } else { ?>
-            <li><a href="<?php echo tep_href_link(FILENAME_LOGIN, '', 'SSL'); ?>"><?php echo HEADER_ACCOUNT_LOGIN; ?></a></li>
-            <li><a href="<?php echo tep_href_link(FILENAME_CREATE_ACCOUNT, '',
-        'SSL');
-    ?>"><?php echo HEADER_ACCOUNT_REGISTER; ?></a></li>
-<?php } ?>
+            <li><a href="<?php echo tep_href_link(FILENAME_LOGOFF, '', 'SSL'); ?>"><i class="fa fa-sign-out"></i> <?php echo _('Log Off'); ?></a></li>
+        <?php } else { ?>
+            <li><a href="<?php echo tep_href_link(FILENAME_LOGIN, '', 'SSL'); ?>"><i class="glyphicon glyphicon-log-in"></i> <?php echo _('Log In'); ?></a></li>
+            <li><a href="<?php
+                echo tep_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL');
+                ?>"><i class="fa fa-pencil"></i> <?php echo _('Register') ?></a></li>
+               <?php } ?>
 
         <li class="divider"></li>
-        <li><a href="<?php echo tep_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>"><?php echo HEADER_ACCOUNT; ?></a></li>
-        <li><a href="<?php echo tep_href_link(FILENAME_ACCOUNT_HISTORY, '',
-                   'SSL');
-?>"><?php echo HEADER_ACCOUNT_HISTORY; ?></a></li>
-        <li><a href="<?php echo tep_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'); ?>"><?php echo HEADER_ACCOUNT_ADDRESS_BOOK; ?></a></li>
-        <li><a href="<?php echo tep_href_link(FILENAME_ACCOUNT_PASSWORD, '',
-                   'SSL');
-?>"><?php echo HEADER_ACCOUNT_PASSWORD; ?></a></li>
+        <li><a href="<?php echo tep_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>"><?php echo _('My Account'); ?></a></li>
+        <li><a href="<?php
+            echo tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL');
+            ?>"><?php echo _('My Orders'); ?></a></li>
+        <li><a href="<?php echo tep_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'); ?>"><?php echo _('My Address Book'); ?></a></li>
+        <li><a href="<?php
+            echo tep_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL');
+            ?>"><?php echo _('My Password'); ?></a></li>
 
     </ul>
 </li>
