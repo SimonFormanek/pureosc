fc5544927d4238063a6c9d51fa04a69d96be600c
Simon Formanek <mail@simonformanek.cz>
2019-02-20 14:19:37 +0100

    removed old links

--- catalog/includes/modules/content/footer/templates/cm_footer_information_links.php
+++ catalog/includes/modules/content/footer/templates/cm_footer_information_links.php
@@ -3,13 +3,6 @@
         <h2><?php echo _('Information'); ?></h2>
         <ul class="nav nav-pills nav-stacked">
             <?php echo tep_information_show_category(1); ?>
-            <?php
-            //echo  '<li><a href="' . tep_href_link('contact_us.php') . '">' . _('Contact Us') . '</a></li>';
-
-            /*             * * Begin Article Manager *** */
-            echo $aLinks;
-            /*             * * End Article Manager *** */
-            ?>
         </ul>  
     </div>
 </div>
