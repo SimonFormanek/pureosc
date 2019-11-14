5c58219967116c03e8a9ba9c9973708868670155
SimonFormanek <mail@simonformanek.cz>
2019-11-14 20:17:06 +0100

    FIX:  catalog/includes/modules/boxes/templates/information.php

--- catalog/includes/modules/boxes/templates/information.php
+++ catalog/includes/modules/boxes/templates/information.php
@@ -1,14 +1,3 @@
 <div class="panel panel-default">
-    <div class="panel-heading"><?php echo MODULE_BOXES_INFORMATION_BOX_TITLE; ?></div>
-    <div class="panel-body">
-        <ul class="list-unstyled">
-            <li><a href="<?php echo tep_href_link('shipping.php'); ?>"><?php echo MODULE_BOXES_INFORMATION_BOX_SHIPPING; ?></a></li>
-            <li><a href="<?php echo tep_href_link('privacy.php'); ?>"><?php echo MODULE_BOXES_INFORMATION_BOX_PRIVACY; ?></a></li>
-            <li><a href="<?php echo tep_href_link('conditions.php'); ?>"><?php echo MODULE_BOXES_INFORMATION_BOX_CONDITIONS; ?></a></li>
-            <li><a href="<?php echo tep_href_link('contact_us.php'); ?>"><?php echo MODULE_BOXES_INFORMATION_BOX_CONTACT; ?></a></li>
-            <?php /*             * * Altered for CCGV ** */ ?>
-            <li><a href="<?php echo tep_href_link('gv_faq.php'); ?>"><?php echo MODULE_BOXES_INFORMATION_GV; ?></a></li>
-            <?php /*             * * EOF alterations for CCGV ** */ ?>
-        </ul>
-    </div>
+     <div class="panel-body"><?php echo $data; ?></div>
 </div>
